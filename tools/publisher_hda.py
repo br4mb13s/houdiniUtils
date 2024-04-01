5283e6bce9c9f119524df9e401eb639df555a570
br4mb13s <116603112+br4mb13s@users.noreply.github.com>
2024-04-01 16:08:50 +1100

    updated hda code with google drive user prefs for hou

--- tools/publisher_hda.py
+++ tools/publisher_hda.py
@@ -10,9 +10,8 @@ class Publisher:
         hou.pwd().parm("file").set("$HIP/geo/$HIPNAME.$OS.$F.bgeo.sc")
         default_path = hou.pwd().parm("file").evalAsString()
 
-        #Grabs the publish location based on the users ENV prefs (broken atm)
-        #dest_path = "$PUBLISH_LOCATION"
-        dest_path = "G:/My Drive/dev/production_assets"
+        #Save location derived from user environment variable
+        dest_path = $GOOGLE_DRIVE
 
         try:
             file_version = len([file for file in os.listdir(dest_path) if file.endswith("bgeo.sc")])+1
@@ -21,5 +20,5 @@ class Publisher:
             file_version = len([file for file in os.listdir(dest_path) if file.endswith("bgeo.sc")])+1
         print("Setting file version: ", file_version)
 
-        _new_file_path = f"$PUBLISH_LOCATION/$HIPNAME.$USER_ID.asset_v{file_version}.bgeo.sc"
+        _new_file_path = f"{dest_path}/$HIPNAME.$USER_ID.asset_v{file_version}.bgeo.sc"
         hou.pwd().parm("file").set(_new_file_path)
